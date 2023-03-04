f82503a5ea9380e07fa93931bf9471ce41ff0474
mkfoo <89305338+mkfoo@users.noreply.github.com>
2023-03-04 11:53:41 +0200

    comment and reorganize header

--- src/ulcd.h
+++ src/ulcd.h
@@ -1,27 +1,47 @@
 #include <stdio.h>
 
+/** Clear display **/
 #define LCD_CMD_CLEAR 0x01
-#define LCD_CMD_HOME 0x02
-#define LCD_CMD_ENTRY_MODE 0x04
-#define LCD_CMD_DISPLAY_CTL 0x08
-#define LCD_CMD_CURSOR_SHIFT 0x10
-#define LCD_CMD_FUNCTION_SET 0x20
-#define LCD_CMD_CGRAM_ADDR 0x40
-#define LCD_CMD_DDRAM_ADDR 0x80
 
-#define LCD_FLAG_SHIFT_DISPLAY 0x01
+/** Return home **/
+#define LCD_CMD_HOME 0x02
+
+/** Entry mode set **/
+#define LCD_CMD_ENTRY_MODE 0x04
 #define LCD_FLAG_DECREMENT 0x00
 #define LCD_FLAG_INCREMENT 0x02
-#define LCD_FLAG_BLINK_ON 0x01
-#define LCD_FLAG_BLINK_OFF 0x00
-#define LCD_FLAG_CURSOR_ON 0x02
-#define LCD_FLAG_CURSOR_OFF 0x00
+#define LCD_FLAG_SHIFT_DISPLAY 0x01
+
+/** Display on/off control **/
+#define LCD_CMD_DISPLAY_CTL 0x08
 #define LCD_FLAG_DISPLAY_ON 0x04
 #define LCD_FLAG_DISPLAY_OFF 0x00
-#define LCD_FLAG_MOVE_RIGHT 0x04
-#define LCD_FLAG_MOVE_LEFT 0x00
+#define LCD_FLAG_CURSOR_ON 0x02
+#define LCD_FLAG_CURSOR_OFF 0x00
+#define LCD_FLAG_BLINK_ON 0x01
+#define LCD_FLAG_BLINK_OFF 0x00
+
+/** Cursor or display shift **/
+#define LCD_CMD_CURSOR_SHIFT 0x10
 #define LCD_FLAG_DISPLAY_MOVE 0x08
 #define LCD_FLAG_CURSOR_MOVE 0x00
+#define LCD_FLAG_MOVE_RIGHT 0x04
+#define LCD_FLAG_MOVE_LEFT 0x00
+
+/** Function set **/
+#define LCD_CMD_FUNCTION_SET 0x20
+/** NB: This library only supports 4-bit mode **/
+#define LCD_FLAG_4_BIT 0x00
+#define LCD_FLAG_2_LINES 0x08
+#define LCD_FLAG_1_LINE 0x00
+#define LCD_FLAG_5X10_FONT 0x04
+#define LCD_FLAG_5X8_FONT 0x00
+
+/** Set CGRAM address **/
+#define LCD_CMD_CGRAM_ADDR 0x40
+
+/** Set DDRAM address **/
+#define LCD_CMD_DDRAM_ADDR 0x80
 
 #define LCD_DEFAULT_GPIO_DEV "/dev/gpiochip0"
 #define LCD_DEFAULT_FIFO_PATH "/var/run/lcd0"
@@ -72,11 +92,11 @@ int lcd_write_raw_stream(int fd, FILE* stream);
 int lcd_write_utf8_stream(int fd, FILE* stream);
 
 /**
- * Execute a command on the LCD device.
+ * Execute an instruction on the LCD device.
  *
  * @param fd An open file descriptor returned by lcd_init.
- * @param cmd A bit pattern to be written into the the LCD's data bus.
- * See the HD44780 data sheet for info about the different commands. 
+ * @param cmd An 8-bit pattern to be written into the the LCD's data bus.
+ * See the HD44780 data sheet for more info about the different instructions. 
  * @return 0 on success, LCD_ERR on failure.
  */
 int lcd_command(int fd, unsigned int cmd);
